a28c9c5d18b9a19239e0af1687580af89f906477
Samridh29 <samridhagarwal011@gmail.com>
2022-07-05 02:27:12 +0530

    Modifiied for concave images

--- objectExtraction.py
+++ objectExtraction.py
@@ -18,15 +18,29 @@ display_image(gray, 'Gray Image')
 edge = cv2.Canny(image, 100, 350)
 display_image(edge, 'Edge Image')
 
-print(image[0, ])
+points=[]
+ap =[] 
+for i in range(h):
+    for j in range(w-1):
+        if edge[i][j] == 255:
+            # points.append([i,j])
+            if(edge[i][j+1] == 255 or edge[i][j-1] == 255):
+                ap.append([i,j])    
+            if((edge[k][j] != 255 for k in range(i+1,h))):
+                points.append([i,j])
 
-points = []
-for i in range(0, h):
-    for j in range(0, w):
-        if edge[i, j] == 255:
-            points.append((i, j))
+black_image = np.zeros((h, w), dtype=np.uint8)
+for point in points:
+    black_image[point[0]][point[1]] = 255
+display_image(black_image, 'Black Image')
 
-display_image(image, 'Black Image')
+
+# After the latest edit
+black_image = np.zeros((h, w), dtype=np.uint8)
+for i in ap:
+    black_image[i[0]][i[1]] = 255
+display_image(black_image, 'Black Image')
+
 # thresh = cv2.threshold(gray, 224, 255, cv2.THRESH_BINARY_INV)[1]
 # display_image(thresh, 'Threshold')
 
